b82d1c0fc0505306b9d435b793e46799b90307ef
Pedro-Lucas-Leal <104956862+Pedro-Lucas-Leal@users.noreply.github.com>
2023-02-03 23:26:15 -0300

    Create 1036.py

---
+++ 1036.py
@@ -0,0 +1,14 @@
+import math
+linhaUm = input().split()
+
+A = float(linhaUm[0])
+B = float(linhaUm[1])
+C = float(linhaUm[2])
+
+delta = (B*B) - (4*A*C)
+
+r1 = (-B + math.sqrt(delta))/ (2*A)
+r2 = -B - math.sqrt(delta)/ (2*A)
+
+print(f'R1 = {r1:.5f}')
+print(f'R2 = {r2:.5f}')
