d8821d813bf84b90d56e8776a7dd20e3189b3fad
Pedro-Lucas-Leal <104956862+Pedro-Lucas-Leal@users.noreply.github.com>
2023-02-03 23:12:38 -0300

    Create 1021.py

---
+++ 1021.py
@@ -0,0 +1,21 @@
+import math
+valor = float(input())
+while valor < 0 or valor > 1000000.00:
+    valor = float(input())
+
+notas = [100, 50, 20, 10, 5, 2]
+moedas = [1.0, 0.50, 0.25, 0.10, 0.05, 0.01]
+print('NOTAS:')
+
+for i in notas:
+    total = int(valor/i)
+    print(f'{total} nota(s) de R$ {i:.2f}')
+    valor = valor - i*total
+
+print('MOEDAS:')
+for x in moedas:
+    valor = round(valor, 2)
+    todo = float(valor/x)
+    todo = math.floor(todo)
+    print(f'{todo} moeda(s) de R$ {x:.2f}')
+    valor = valor - todo*x
