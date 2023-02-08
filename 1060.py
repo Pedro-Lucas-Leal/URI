93cce62ebd6f1485101776cb784693bfffe3c10f
Pedro-Lucas-Leal <104956862+Pedro-Lucas-Leal@users.noreply.github.com>
2023-02-08 20:35:14 -0300

    Create 1060.py

---
+++ 1060.py
@@ -0,0 +1,16 @@
+num1 = float(input())
+num2 = float(input())
+num3 = float(input())
+num4 = float(input())
+num5 = float(input())
+num6 = float(input())
+i = 0
+
+if num1 > 0: i = i + 1
+if num2 > 0: i = i + 1
+if num3 > 0: i = i + 1
+if num4 > 0: i = i + 1
+if num5 > 0: i = i + 1
+if num6 > 0: i = i + 1
+
+print(f'{i} valores positivos')
