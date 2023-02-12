7f682c3b1ef5b6030312de5c8cd17dcfdf349cd3
Pedro-Lucas-Leal <104956862+Pedro-Lucas-Leal@users.noreply.github.com>
2023-02-12 16:04:58 -0300

    Update 1047.py

--- 1047.py
+++ 1047.py
@@ -1,5 +1,3 @@
-#                                                                        code 1
-
 entrada = input().split()
 horaInicial = int(entrada[0])
 minutoInicial = int(entrada[1])
@@ -8,14 +6,17 @@ minutoFinal = int(entrada[3])
 
 if horaInicial == horaFinal and minutoInicial == minutoFinal:
     print('O JOGO DUROU 24 HORA(S) E 0 MINUTO(S)')
+elif horaInicial == horaFinal and minutoInicial < minutoFinal:
+    print(f'O JOGO DUROU 0 HORA(S) E {minutoFinal - minutoInicial} MINUTO(S)')
 else:
     if horaInicial < horaFinal:
         resultadoHora = horaFinal - horaInicial
     else:
-        resultadoHora = abs(horaFinal - 24 - horaInicial)
+        resultadoHora = (24 - horaInicial) + horaFinal
     if minutoInicial > minutoFinal:
-        resultadoMinuto = abs(minutoInicial - 60 - minutoFinal)
+        resultadoMinuto = (60 - minutoInicial) + minutoFinal
         resultadoHora = resultadoHora - 1
     else:
         resultadoMinuto = minutoFinal - minutoInicial
-    print(f'O JOGO DUROU {resultadoHora} HORA(S) E {resultadoMinuto} MINUTOS(S)')
+    print(
+        f'O JOGO DUROU {resultadoHora} HORA(S) E {resultadoMinuto} MINUTO(S)')
